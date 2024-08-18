fca7cbf5e8d7e4a5b2c8a4937e7b2797c4c54e77
SalimovUmidbek <salimovumidbek267@gmail.com>
2024-08-18 13:34:00 +0500

    bular ozgardi

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import logo from '../src/assets/icontelegram.webp'
+import rasm from '../src/assets/logo.png'
 
 const Footer = () => {
   return (
     <div>
-      <div>
+      <div className='flex items-center gap-[30px] ben'>
         <div>
-          <button className='w-[139px] h-[41px] bg-black rounded-[10px]' >
+          <button className='w-[139px] h-[41px] bg-black rounded-[10px] why' >
             <Link className='text-white flex items-center justify-evenly' to={'https://t.me/im_slmv'}><img className='w-[30px] h-[30px]' src={logo} alt="" /> telegram</Link>
           
             
@@ -18,16 +19,28 @@ const Footer = () => {
           <div className='flex abi'>
           
             <NavLink className={'w-[100px]'} to={'/about'}>Biz haqimizda</NavLink>
-            <NavLink className={'w-[100px]'} to={'/products'}>Menyu</NavLink>
-            <NavLink className={'w-[100px]'} to={'/contact'}>Aksiyalar</NavLink>
-            <NavLink className={'w-[100px]'} to={'/filiallar'}>Filiallar</NavLink>
-            <NavLink className={'w-[100px]'} to={'/kontaktlar'}>Kontaktlar</NavLink>
+            <NavLink className={'w-[50px]'} to={'/products'}>Menyu</NavLink>
+            <NavLink className={'w-[65px]'} to={'/contact'}>Aksiyalar</NavLink>
+            <NavLink className={'w-[50px]'} to={'/filiallar'}>Filiallar</NavLink>
+            <NavLink className={'w-[75px]'} to={'/kontaktlar'}>Kontaktlar</NavLink>
             
           
             {/* <NavLink to={'/'}>home</NavLink> */}
           </div>
         </div>
       </div>
+      <hr />
+      
+        <div className='flex justify-between items-center falo'>
+          <div>
+            <h1>2024 Barcha huquqlar himoyalangan</h1>
+          </div>
+          <img className='w-[197px] h-[50px]' src={rasm} alt="" />
+          <div>
+            <h1>Project by: Salimov Umidbek</h1>
+          </div>
+        </div>
+      <hr />
     </div>
   )
 }
--- src/pages/Kontaktlar.jsx
+++ src/pages/Kontaktlar.jsx
@@ -3,40 +3,40 @@ import React from 'react'
 const Kontaktlar = () => {
   return (
     <div className='kant'>
-      <div className="text-3xl pt-[20px]">Biz haqimizda</div>
+      <div className="text-3xl pt-[20px]">Kontaktlar</div>
       <div class="border-b-2 w-24 mb-10 border-[#fc004a] pt-[10px]"></div>
       <div>
         <h1 className='pb-[10px]'>+998712004242</h1>
         <h1>Ish vaqti: har kuni soat 10-00 dan 02-45 gacha</h1>
       </div>
 
-      <div className='w-[724px] h-[442px] border border-gray-400 rounded-2xl p-8 md:w-[724px]'>
+      <div className='w-[724px] h-[442px] border border-gray-400 rounded-2xl p-8 md:w-[724px] dunno'>
         <h1 className='text-[30px]'>Sharhingizni qoldiring</h1>
-        <div className='flex justify-between'>
+        <div className='flex justify-between byebye'>
           <div>
             <h1>Ism</h1>
             {/* <input type="text" name='name' className='bg-gray-100 ' /> */}
-            <input type="text" name="name" class="bg-gray-100 px-8 py-2 rounded-xl outline-none focus:outline-none w-[320px] h-[41px]">
+            <input type="text" name="name" class="bg-gray-100 px-8 py-2 rounded-xl outline-none focus:outline-none w-[320px] h-[41px] ban">
             </input>
           </div>
           <div>
             <h1>Telefon raqamingiz</h1>
             {/* <input type="text" name='name' className='bg-gray-100 ' /> */}
-            <input type="text" name="name" class="bg-gray-100 px-8 py-2 rounded-xl outline-none focus:outline-none w-[320px] h-[41px]" placeholder='+998 '>
+            <input type="text" name="name" class="bg-gray-100 px-8 py-2 rounded-xl outline-none focus:outline-none w-[320px] h-[41px] ban" placeholder='+998 '>
             </input>
           </div>
         </div>
-        <div className='flex justify-between mt-[50px]'>
+        <div className='flex justify-between mt-[50px] byebye'>
           <div>
             <h1>Email Pochtangiz</h1>
             {/* <input type="text" name='name' className='bg-gray-100 ' /> */}
-            <input type="text" name="name" class="bg-gray-100 px-8 py-2 rounded-xl outline-none focus:outline-none w-[320px] h-[41px]">
+            <input type="text" name="name" class="bg-gray-100 px-8 py-2 rounded-xl outline-none focus:outline-none w-[320px] h-[41px] ban">
             </input>
           </div>
           <div>
             <h1>Buyurtma soni</h1>
             {/* <input type="text" name='name' className='bg-gray-100 ' /> */}
-            <input type="text" name="name" class="bg-gray-100 px-8 py-2 rounded-xl outline-none focus:outline-none w-[320px] h-[41px]" placeholder='Misol uchun: 1,2,3,4 '>
+            <input type="text" name="name" class="bg-gray-100 px-8 py-2 rounded-xl outline-none focus:outline-none w-[320px] h-[41px] ban" placeholder='Misol uchun: 1,2,3,4 '>
             </input>
           </div>
         </div>
